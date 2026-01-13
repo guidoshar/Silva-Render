69f4f3b6a8fb3f481de7bcb9cbd028d19da61563
Guido <guidoshar@126.com>
2026-01-13 16:30:31 +0800

    ChatBUllbe增加输出演示

--- src/types/ui-block.ts
+++ src/types/ui-block.ts
@@ -1,3 +1,4 @@
+// --- 1.Callout ---
 export type UICallout = {
     kind: "callout";
     tone?: "info" | "success" | "warning" | "danger";
@@ -5,15 +6,23 @@ export type UICallout = {
     text: string;
 };
 
+// --- 2.Text ---
+export type UIText = {
+    kind: "text";
+    text: string;
+};
+
+// --- 3.List ---
 export type UIlist ={
     kind: "list";
     title?: string;
     items: string[];
 };
 
-export type UIText = {
-    kind: "text";
-    text: string;
-};
-
-export type UIBlock = UICallout | UIlist | UIText;
+// --- 4.Audio ---
+export type UIAudio ={
+    kind: "audio";
+    caption?: string; //介绍语
+    url: string | null; //音频链接，null表示生成失败
+}
+export type UIBlock = UICallout | UIlist | UIText | UIAudio;
